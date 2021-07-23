f4b88e1cafee4741135129599f8178e3e4bf880a
Vitor <vi.souza.almeida@protonmail.com>
2021-07-23 09:45:09 -0300

    Organização de pastas

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    pTag = document.querySelector("div");
-    newVal = document.createElement("p");
-    newVal.innerHTML = '';
-    pTag.appendChild(newVal);
-  });
